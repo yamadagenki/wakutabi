31855e520cb05bcb87b70b9fe74dab6bdd5d31e9
Shengbo.Xu <shengbo.xu@medley.jp>
2017-10-13 23:43:40 +0900

    add: contents 2017年 10月13日 金曜日 23時43分40秒 JST

--- _plugins/slug2name.rb
+++ _plugins/slug2name.rb
@@ -6,7 +6,9 @@ module Jekyll
         'y2018' => '2018年おすすめ',
         'tokyo' => '東京近郊',
         'spring' => '温泉',
-        'lady' => '女子旅'
+        'lady' => '女子旅',
+        'abroad' => '海外旅行',
+        'man' => '男子旅'
       }
       input.map(&:to_s).uniq.map{|item| translate[item]}.join(',')
     end
