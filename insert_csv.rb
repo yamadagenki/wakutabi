21d090d73da1a44bb7449ef9b16f03fc285d49a1
Shengbo.Xu <shengbo.xu@medley.jp>
2017-10-25 01:49:18 +0900

    add: contents 2017年 10月25日 水曜日 01時49分18秒 JST

--- insert_csv.rb
+++ insert_csv.rb
@@ -20,8 +20,8 @@ def new_line(line)
 end
 
 CSV.foreach(filename, :col_sep => "\t", :headers => true) do |row|
-  next if Pages.exists?(title: row[3])
-  total_body = (3..15).step(2).to_a.map{|i| row[i]}.join('').tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z')
+  next if Pages.exists?(title: row[5])
+  total_body = (5..29).step(2).to_a.map{|i| row[i]}.join('').tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z')
   category = []
   category << 'y2018' if total_body.match(/2018/)
   category << 'lady' if total_body.match(/女/)
@@ -31,13 +31,13 @@ CSV.foreach(filename, :col_sep => "\t", :headers => true) do |row|
   category << 'tokyo' if total_body.match(/東京|千葉|埼玉|神奈川|群馬|栃木|山梨/)
   category << 'abroad' if total_body.match(/海外|国外|外国|ヨーロッパ|アメリカ|アジア/)
   Pages.create(
-    title: row[4].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
+    title: row[5].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
     category: category.join(','),
-    sub_title1: row[7].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
-    body1: new_line(row[10]),
-    sub_title2: row[13].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
-    body2: new_line(row[16]),
-    sub_title3: row[19].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
-    body3: new_line(row[22])
+    sub_title1: row[9].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
+    body1: new_line(row[13]),
+    sub_title2: row[17].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
+    body2: new_line(row[21]),
+    sub_title3: row[25].tr('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z'),
+    body3: new_line(row[29])
   )
 end
